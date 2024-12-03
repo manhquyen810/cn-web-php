4936f487b0ed2bbe0316b8fd4917aceb3bcfd058
manhquyen810 <nguyenmanhquyen2712@gmail.com>
2024-12-03 17:14:54 +0700

    init 27/11

---
+++ BTVN/b.27-11/products_management/products.sql
@@ -0,0 +1,6 @@
+create table products (
+                          id INT AUTO_INCREMENT primary key,
+                          name VARCHAR(50),
+                          price DECIMAL(7,2)
+);
+insert into products (id, name, price) values (1, 'Lari', 1537.84);
